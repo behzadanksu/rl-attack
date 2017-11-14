61da9ee89168f6f57354a9b03e8d7bdd3623dadb
Vahid Behzadan <behzadan@ksu.edu>
2017-11-14 04:42:34 -0600

    Fixed model.py

--- model.py
+++ model.py
@@ -1,15 +1,8 @@
 import tensorflow as tf
 import tensorflow.contrib.layers as layers
+from baselines.common.tf_util import noisy_dense
 
-
-def layer_norm_fn(x, relu=True):
-    x = layers.layer_norm(x, scale=True, center=True)
-    if relu:
-        x = tf.nn.relu(x)
-    return x
-
-
-def model(img_in, num_actions, scope, reuse=False, layer_norm=False, concat_softmax=False):
+def model(img_in, num_actions, scope, noisy=False, reuse=False, concat_softmax=False):
     """As described in https://storage.googleapis.com/deepmind-data/assets/papers/DeepMindNature14236Paper.pdf"""
     with tf.variable_scope(scope, reuse=reuse):
         out = img_in
@@ -18,21 +11,24 @@ def model(img_in, num_actions, scope, reuse=False, layer_norm=False, concat_soft
             out = layers.convolution2d(out, num_outputs=32, kernel_size=8, stride=4, activation_fn=tf.nn.relu)
             out = layers.convolution2d(out, num_outputs=64, kernel_size=4, stride=2, activation_fn=tf.nn.relu)
             out = layers.convolution2d(out, num_outputs=64, kernel_size=3, stride=1, activation_fn=tf.nn.relu)
-        conv_out = layers.flatten(out)
+        out = layers.flatten(out)
 
         with tf.variable_scope("action_value"):
-            value_out = layers.fully_connected(conv_out, num_outputs=512, activation_fn=None)
-            if layer_norm:
-                value_out = layer_norm_fn(value_out, relu=True)
+            if noisy:
+                # Apply noisy network on fully connected layers
+                # ref: https://arxiv.org/abs/1706.10295
+                out = noisy_dense(out, name='noisy_fc1', size=512, activation_fn=tf.nn.relu)
+                out = noisy_dense(out, name='noisy_fc2', size=num_actions)
             else:
-                value_out = tf.nn.relu(value_out)
-            value_out = layers.fully_connected(value_out, num_outputs=num_actions, activation_fn=None)
+                out = layers.fully_connected(out, num_outputs=512, activation_fn=tf.nn.relu)
+                out = layers.fully_connected(out, num_outputs=num_actions, activation_fn=None)
+            #V: Softmax - inspired by deep-rl-attack #
             if concat_softmax:
-                value_out = tf.nn.softmax(value_out)
-        return value_out
+                out = tf.nn.softmax(out)
+        return out
 
 
-def dueling_model(img_in, num_actions, scope, reuse=False, layer_norm=False, concat_softmax=False):
+def dueling_model(img_in, num_actions, scope, noisy=False, reuse=False, concat_softmax=False):
     """As described in https://arxiv.org/abs/1511.06581"""
     with tf.variable_scope(scope, reuse=reuse):
         out = img_in
@@ -41,22 +37,27 @@ def dueling_model(img_in, num_actions, scope, reuse=False, layer_norm=False, con
             out = layers.convolution2d(out, num_outputs=32, kernel_size=8, stride=4, activation_fn=tf.nn.relu)
             out = layers.convolution2d(out, num_outputs=64, kernel_size=4, stride=2, activation_fn=tf.nn.relu)
             out = layers.convolution2d(out, num_outputs=64, kernel_size=3, stride=1, activation_fn=tf.nn.relu)
-        conv_out = layers.flatten(out)
+        out = layers.flatten(out)
 
         with tf.variable_scope("state_value"):
-            state_hidden = layers.fully_connected(conv_out, num_outputs=512, activation_fn=None)
-            if layer_norm:
-                state_hidden = layer_norm_fn(state_hidden, relu=True)
+            if noisy:
+                # Apply noisy network on fully connected layers
+                # ref: https://arxiv.org/abs/1706.10295
+                state_hidden = noisy_dense(out, name='noisy_fc1', size=512, activation_fn=tf.nn.relu)
+                state_score = noisy_dense(state_hidden, name='noisy_fc2', size=1)
             else:
-                state_hidden = tf.nn.relu(state_hidden)
-            state_score = layers.fully_connected(state_hidden, num_outputs=1, activation_fn=None)
+                state_hidden = layers.fully_connected(out, num_outputs=512, activation_fn=tf.nn.relu)
+                state_score = layers.fully_connected(state_hidden, num_outputs=1, activation_fn=None)
         with tf.variable_scope("action_value"):
-            actions_hidden = layers.fully_connected(conv_out, num_outputs=512, activation_fn=None)
-            if layer_norm:
-                actions_hidden = layer_norm_fn(actions_hidden, relu=True)
+            if noisy:
+                # Apply noisy network on fully connected layers
+                # ref: https://arxiv.org/abs/1706.10295
+                actions_hidden = noisy_dense(out, name='noisy_fc1', size=512, activation_fn=tf.nn.relu)
+                action_scores = noisy_dense(actions_hidden, name='noisy_fc2', size=num_actions)
             else:
-                actions_hidden = tf.nn.relu(actions_hidden)
-            action_scores = layers.fully_connected(actions_hidden, num_outputs=num_actions, activation_fn=None)
+                actions_hidden = layers.fully_connected(out, num_outputs=512, activation_fn=tf.nn.relu)
+                action_scores = layers.fully_connected(actions_hidden, num_outputs=num_actions, activation_fn=None)
             action_scores_mean = tf.reduce_mean(action_scores, 1)
             action_scores = action_scores - tf.expand_dims(action_scores_mean, 1)
+
         return state_score + action_scores
